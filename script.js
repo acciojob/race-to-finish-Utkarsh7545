ad8ccd517872b7046ebec8629bb8779a430c8cc9
Utkarsh Kumar Patel <88320546+Utkarsh7545@users.noreply.github.com>
2023-03-18 00:07:35 +0530

    created a submission

--- script.js
+++ script.js
@@ -2,3 +2,16 @@ window.promises = [];
 
 // Do not change the code above this
 // add your promises to the array `promises`
+
+const x = document.getElementById("output");
+window.promises = Array.from({length: 5}=> 
+	new Promise((resolve, reject)=>{
+		const time=Math.floor(Math.random()*5)+1;
+	setTimeout(()=>{
+		resolve(${time});
+	},time*1000);
+}));
+
+Promise.any(window.promises)
+	.then((element)=>x.innerHTML=element;
+		)
